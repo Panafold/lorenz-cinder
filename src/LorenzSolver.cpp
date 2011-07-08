e95b9abff29da9544973d4d846b75ea63a65509d
Guillaume Ardaud <guillaume.ardaud@gmail.com>
2011-07-08 10:24:53 -0700

    Prettier constructor

--- src/LorenzSolver.cpp
+++ src/LorenzSolver.cpp
@@ -1,17 +1,35 @@
-//
-//  LorenzSolver.cpp
-//
-//  Created by Guillaume on 7/7/11. guillaume@panafold.com
-//  Copyright 2011 Panafold. All rights reserved.
-//
+/*
+ LorenzSolver.cpp
+ Created by Guillaume Ardaud on 7/7/11. guillaume@panafold.com
+ Copyright 2011 Panafold. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ software and associated documentation files (the "Software"), to deal in the Software 
+ without restriction, including without limitation the rights to use, copy, modify, 
+ merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ permit persons to whom the Software is furnished to do so, subject to the following 
+ conditions:
+ 
+ The above copyright notice and this permission notice shall be included in 
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+ OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ IN THE SOFTWARE.
+
+ */
 
 #include "LorenzSolver.h"
 
-LorenzSolver::LorenzSolver(){
-    this->delta=0.01f;
-    this->sigma=10.f;
-    this->beta=8.f/3.f;
-    this->rho=28.0f;
+LorenzSolver::LorenzSolver(float d, float s, float b, float r){
+    this->delta=d;
+    this->sigma=s;
+    this->beta=b;
+    this->rho=r;
 }
 
 Vec3f LorenzSolver::computePoint(Vec3f oldPoint){
